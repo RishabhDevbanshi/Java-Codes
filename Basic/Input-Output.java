7b02cf59467935582135c11b954f2864e8d1b8bc
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2020-10-16 20:04:06 +0530

    Create Input-Output.java

---
+++ Basic/Input-Output.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	   System.out.println("What's your name?");
+	   Scanner scanner = new Scanner(System.in);
+	   String name = scanner.nextLine();
+	   
+	   System.out.println("Hello " + name);
+	}
+}
