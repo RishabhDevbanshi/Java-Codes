01b4164579d2a0594dbbd6c0d49ca85965dce339
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2020-10-16 20:06:17 +0530

    Update Input-Output.java
    Added syntax

--- Basic/Input-Output.java
+++ Basic/Input-Output.java
@@ -5,6 +5,8 @@ class Codechef
 	public static void main (String[] args) throws java.lang.Exception
 	{
 	   System.out.println("What's your name?");
+           
+	   //Syntax => type identifier = type();
 	   Scanner scanner = new Scanner(System.in);
 	   String name = scanner.nextLine();
 	   
