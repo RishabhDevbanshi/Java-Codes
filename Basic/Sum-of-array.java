005886e18f9e91d225ca375ececd6844a629a5ac
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2020-10-18 07:35:32 +0530

    Update Sum-of-array.java

--- Basic/Sum-of-array.java
+++ Basic/Sum-of-array.java
@@ -1,47 +1,46 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class Main{
 
-    public static void main(String[] args) throws Exception {
-        Scanner scn = new Scanner(System.in);
-        int n1 = scn.nextInt();
-        int[] arr1 = new int[n1];
-        for (int i = 0; i < n1; i++) {
-            arr1[i] = scn.nextInt();
-        }
-        int n2 = scn.nextInt();
-        int[] arr2 = new int[n2];
-        for (int i = 0; i < n2; i++) {
-            arr2[i] = scn.nextInt();
-        }
-        int c = Math.max(arr1.length, arr2.length);
-        if(c==arr1.length)
-           {arr2=reverse(arr2);}
-        else
-           {arr1=reverse(arr1);}
-        int[] arr = new int[c];
-        for (int i = 0; i < c; i++) {
-            if (arr1.length > i)
-                arr[i] += arr1[i];
-            if (arr2.length > i)
-                arr[i] += arr2[i];
-        }
-        arr = reverse(arr);
-        for (int x: arr) {
-            System.out.println(x);
-        }
-    }
-    public static int[] reverse(int a[])
+public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n1 = scn.nextInt();
+    int[] arr1 = new int[n1];
+    for(int i=0;i<n1;i++)
     {
-        int[] reversedArray = new int[a.length];
-        int j = a.length;
-        for(int i=0;i<a.length;i++)
-        {
-            reversedArray[j-1] = a[i];
-            j--;
-        }
-        return reversedArray;
+        arr1[i] = scn.nextInt();
     }
+    int n2 = scn.nextInt();
+    int[] arr2 = new int[n2];
+    for(int i=0;i<n2;i++)
+    {
+        arr2[i] = scn.nextInt();
+    }
+    int[] sum = new int[n1>n2?n1:n2];
+    int carry = 0;
+    int i = arr1.length - 1;
+    int j = arr2.length - 1;
+    int k = sum.length - 1;
+    while(k>=0)
+    {
+        int digit = carry;
+        if(i>=0)
+          digit += arr1[i];
+        if(j>=0)
+           digit += arr2[j];
+        
+        carry = digit/10;
+        digit%=10;
+        sum[k] = digit;
+        i--;
+        j--;
+        k--;
+    }
+    if(carry > 0)
+      System.out.println(carry);
+    for(int c : sum)
+       System.out.println(c);
+ }
 
 }
