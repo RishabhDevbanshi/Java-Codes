c1da5ce2068688ca8fcaa5ed2fcd451f52d3eecf
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2020-10-19 19:57:19 +0530

    Create Subset-of-array.java

---
+++ Basic/Subset-of-array.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+public static void main(String[] args) throws Exception {
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] arr = new int[n];
+    for(int i=0;i<n;i++)
+    {
+        arr[i]=scn.nextInt();
+    }
+    
+    int limit = (int)Math.pow(2,n);
+    for(int i=0;i<limit;i++)
+    {
+        int temp=i;
+        String set = "";
+        for(int j=arr.length -1 ;j>=0;j--)
+        {
+            int rem = temp%2;
+            temp/=2;
+            if(rem==0)
+            {
+                set = "-\t" + set;
+            }
+            else
+            {
+                set = arr[j]+"\t"+set;
+            }
+        }
+        System.out.println(set);
+    }
+ }
+
+}
