989bb2f24fbb15545034e6abe06777c52bcb89a5
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2020-10-19 11:36:30 +0530

    Create Floor-of-element.java

---
+++ Binary Search/Floor-of-element.java
@@ -0,0 +1,53 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		Scanner scn = new Scanner(System.in);
+		int t = scn.nextInt();
+		while(t>0)
+		{
+		    int n = scn.nextInt();
+		    int ele = scn.nextInt();
+		    int[] arr = new int[n];
+		    for(int i=0;i<n;i++)
+		      arr[i] = scn.nextInt();
+		    
+		    if(arr[0]>ele)
+		    {
+		        System.out.println(-1);
+		        t--;
+		        continue;
+		    }
+		    if(arr[n-1]<ele)
+		    {
+		        System.out.println(n-1);
+		        t--;
+		        continue;
+		    }
+		    int l=0 , r=n-1;
+		    int res=-1;
+		    while(l<=r)
+		    {
+		        int mid = l + (r-l)/2;
+		        if(arr[mid]==ele)
+		        {
+		            res=mid;
+		            break;
+		        }
+		        else if(ele<arr[mid])
+		        {
+		            r=mid-1;
+		        }
+		        else
+		        {
+		            res = mid;
+		            l=mid+1;
+		        }
+		    }
+		    System.out.println(res);
+		    t--;
+		}
+	}
+}
