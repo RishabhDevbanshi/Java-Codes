2a9d847ad32c6bdbd84d2583790b466d671a4642
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2020-10-21 18:16:26 +0530

    Create Rotate-Array-by-90-degree.java

---
+++ 2D_Array/Rotate-Array-by-90-degree.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int[][] arr = new int[n][n];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                arr[i][j] = scn.nextInt();
+            }
+        }
+        rotate90Clockwise(arr,n);
+        display(arr);
+    }
+
+    public static void rotate90Clockwise(int a[][],int N) {
+
+        // Traverse each cycle 
+        for (int i = 0; i < N / 2; i++) {
+            for (int j = i; j < N - i - 1; j++) {
+
+                // Swap elements of each cycle 
+                // in clockwise direction 
+                int temp = a[i][j];
+                a[i][j] = a[N - 1 - j][i];
+                a[N - 1 - j][i] = a[N - 1 - i][N - 1 - j];
+                a[N - 1 - i][N - 1 - j] = a[j][N - 1 - i];
+                a[j][N - 1 - i] = temp;
+            }
+        }
+    }
+
+    public static void display(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
