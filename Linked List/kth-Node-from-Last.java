f0128cf92086392cd5ee2f8d0c1759f16cc6e421
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2020-12-19 19:27:50 +0530

    Update kth-Node-from-Last.java

--- Linked List/kth-Node-from-Last.java
+++ Linked List/kth-Node-from-Last.java
@@ -1,9 +1,29 @@
+//by using size of linked list
+
 public int kthFromLast(int k){
-      k = size-k;
+      k = size-k;  //here size is size of linked list
       Node temp = head;
       for(int i=0;i<k-1;i++)
       {
           temp = temp.next;
       }
       return temp.data;
+    }
+
+//without using size of linked list
+
+
+public int kthFromLast(int k){
+      Node slow = head;
+      Node fast = head;
+      for(int i=0;i<k;i++)
+      {
+          fast = fast.next;
+      }
+      while(fast.next!=null)
+      {
+          slow = slow.next;
+          fast = fast.next;
+      }
+      return slow.data;
     }
