6ae6c94a77e4cf72dee7b23fe843432ad7d50844
Rishabh Devbanshi <64083218+RishabhDevbanshi@users.noreply.github.com>
2020-12-20 12:15:54 +0530

    Create Merge-two-sorted-list.java

---
+++ Linked List/Merge-two-sorted-list.java
@@ -0,0 +1,28 @@
+public static LinkedList mergeTwoSortedLists(LinkedList l1, LinkedList l2) {
+      LinkedList ans = new LinkedList();
+      Node t1=l1.head, t2=l2.head;
+      while(t1!=null && t2!=null)
+      {
+        if(t1.data < t2.data)
+        {
+          ans.addLast(t1.data);
+          t1 = t1.next;
+        }
+        else
+        {
+          ans.addLast(t2.data);
+          t2 = t2.next;
+        }
+      }
+      while(t1!=null)
+      {
+        ans.addLast(t1.data);
+        t1 = t1.next;
+      }
+      while(t2!=null)
+      {
+        ans.addLast(t2.data);
+        t2 = t2.next;
+      }
+      return ans;
+    }
